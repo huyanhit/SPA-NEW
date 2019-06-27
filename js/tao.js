d433c318b955e958cc0b51bd6af369666501d3ab
Anh huy <huyanhit@gmail.com>
2019-06-27 22:24:32 +0700

    update code

---
+++ js/tao.js
@@ -0,0 +1,19 @@
+$(window).load(function() {
+  $('#carousel').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    itemWidth: 110,
+    itemMargin: 5,
+    asNavFor: '#slider'
+  });
+ 
+  $('#slider').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    sync: "#carousel"
+  });
+});
