238ff19a6d0faa7a734724d9039c845746414dbe
Anh huy <huyanhit@gmail.com>
2019-07-03 19:29:01 +0700

    update tao

--- js/tao.js
+++ js/tao.js
@@ -17,3 +17,21 @@ $(window).load(function() {
     sync: "#carousel"
   });
 });
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
